2be61bc29ee7818681412040d4d7b0303ccfa1bb
andihappy <1015126672@qq.com>
2024-04-19 16:49:57 +0800

    go test case

--- language/recover_example_test.go
+++ language/recover_example_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 // 正确示例：
-func Test() {
+func T() {
 	defer func() {
 		if err := recover(); err != nil {
 			debug.PrintStack()
@@ -19,7 +19,7 @@ func Test() {
 }
 
 func TestRecoverExample3(t *testing.T) {
-	go Test()
+	go T()
 	time.Sleep(5 * time.Second)       // 模拟执行耗时任务(顺便等待子协程执行)
 	fmt.Println("main()依然是能正常执行的...") // 可以正常打印，即使Test()发生panic
 }
